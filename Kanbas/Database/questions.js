af6a0f8375eb409c2d7f3228f36e905218a93f41
Maggy <chrismegaman@live.com>
2024-11-30 17:35:42 -0500

    added database for quiz questions, updated with options and answer array to give an explanation

--- Kanbas/Database/questions.js
+++ Kanbas/Database/questions.js
@@ -8,7 +8,10 @@ export default [
         "quizId": 101,
         "points": 10,
         "question": "This is a test answer: True or Yes",
-        "answer": true,
+        "options": [true, false],
+        "answer": [
+            { "value": true, "explanation": "True is correct because..." },
+        ],
         "type": "True/False"
 
     }, 
@@ -17,7 +20,10 @@ export default [
         "quizId": 101,
         "points": 15,
         "question": "This is a test answer: False or No",
-        "answer": false,
+        "options": [true, false],
+        "answer": [
+            { "value": false, "explanation": "false is correct because..." },
+        ],
           "type": "True/False"
     },
     {
@@ -25,6 +31,10 @@ export default [
         "quizId": 101,
         "points": 15,
         "question": "This is a test, answer: 1",
+        "options": [1, 2],
+        "answer": [
+            { "value": 1, "explanation": "1 is correct because..." },
+        ],
         "answer": 1,
           "type": "Fill in the blank"
     },
@@ -33,7 +43,10 @@ export default [
         "quizId": 101,
         "points": 15,
         "question": "This is a test, answer 2",
-        "answer": 2,
+        "options": ["Answer", "Not answer"],
+        "answer": [
+            { "value": "Answer", "explanation": "Answer is correct because..." },
+        ],
         "type": "Fill in the blank"
     },
     {
@@ -41,7 +54,10 @@ export default [
         "quizId": 101,
         "points": 15,
         "question": "This is a test, answer: b",
-        "answer": "b",
+        "options": ["a", "b"],
+        "answer": [
+            { "value": "a", "explanation": "a is correct because..." },
+        ],
         "type": "multi-choice"
     },
     {
@@ -49,7 +65,10 @@ export default [
         "quizId": 101,
         "points": 15,
         "question": "This is a test, answer: a",
-        "answer": "a",
+        "options": ["a", "b"],
+        "answer": [
+            { "value": "b", "explanation": "b is correct because..." },
+        ],
          "type": "multi-choice"
     },
     {
@@ -57,7 +76,24 @@ export default [
         "quizId": 101,
         "points": 15,
         "question": "This is a test, answer : a and b",
-        "answer": ["a", "b"],
+       "options": ["a", "b"],
+        "answer": [
+            { "value": "a", "explanation": "a is correct because..."}, 
+            { "value": "b", "explanation": "b is correct because..."
+            }
+        ],
+         "type": "multi-choice"
+    },
+
+    {
+        "_id": 8,
+        "quizId": 101,
+        "points": 25,
+        "question": "This is a test, answer : a and b, only the last option",
+       "options": ["a", "b", "a and b"],
+        "answer": [
+            { "value": "a and b", "explanation": "a and b is correct because..."}, 
+        ],
          "type": "multi-choice"
     }
 
