5a3aa70aa9a98aa115627e12e1c89b1f8faa6330
Alp Yalcinkaya <yalcinkayaalp@gmail.com>
2024-12-08 16:28:42 -0500

    Change user profile to get method

--- Kanbas/Users/routes.js
+++ Kanbas/Users/routes.js
@@ -84,5 +84,6 @@ export default function UserRoutes(app) {
   app.post("/api/users/signup", signup);
   app.post("/api/users/signin", signin);
   app.post("/api/users/signout", signout);
-  app.post("/api/users/profile", profile);
+  app.get("/api/users/profile", profile);
+  // app.post("/api/users/profile", profile);
 }
